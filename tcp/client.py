e18c0d2ee74440b342ec489cdd14c7e6964edc22
NathanJFeldman <nathanfeldman.init@gmail.com>
2024-10-24 14:27:20 +0200

    eee

--- tcp/client.py
+++ tcp/client.py
@@ -1,4 +1,4 @@
-import socket, select, sys
+import socket, threading
 
 HOST = "127.0.0.1"
 PORT = 12345
@@ -6,13 +6,4 @@ PORT = 12345
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
-    while True:
-        chat : str = input("Enter: ")
-        data, addr = sock.recvfrom(2048)
-        encoded_data = chat.encode("utf-8")
-        sent_message = "{addr}: {encoded_data}"
-        sock.sendto(encoded_data, (HOST, PORT))
-        decoded_message = data.decode("utf-8")
-        
-
-
+    
--- tcp/server.py
+++ tcp/server.py
@@ -8,26 +8,4 @@ chatters : list = []
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind((HOST, PORT))
     sock.listen(10)
-
-    def chat_joined():
-        print("{addr} has connected.")
-        chatters.append(sock)
-        while True:
-            try:
-                message = sock.recv(1024).decode("utf-8")
-                if message:
-                    send_all(message, sock)
-            except:
-                break
-
-
-
-    def send_all(message, sender):
-        for chatter in chatters:
-            if chatter != sender:
-                try:
-                    chatter.send(message.encode("utf-8"))
-                except:
-                    sock.close()
-                    chatters.remove(chatter)
-    def 
+    
