bb12e8d5211ec9feeb615a9130abdc1e2a2e9555
NathanJFeldman <nathanfeldman.init@gmail.com>
2024-10-24 13:58:56 +0200

    new

--- tcp/client.py
+++ tcp/client.py
@@ -5,27 +5,14 @@ PORT = 12345
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
-
+    sock.connect((HOST, PORT))
     while True:
-        data, addr = sock.recvfrom(1024)
         chat : str = input("Enter: ")
+        data, addr = sock.recvfrom(2048)
         encoded_data = chat.encode("utf-8")
         sent_message = "{addr}: {encoded_data}"
         sock.sendto(encoded_data, (HOST, PORT))
         decoded_message = data.decode("utf-8")
+        
 
 
-
-
-    #print("Connected")
-    #sock.connect((HOST, PORT))
-    #message = "Welcome to the chat"
-    #encoded_message = message.encode("utf-8")
-    #sock.sendall(encoded_message)
-    #chat = pyip.inputStr("Type in chat: ")
-#
-    #response = sock.recv(512)
-
-#decoded_response = response.decode("utf-8")
-#print(f"svaret från clientsockn: {decoded_response}") 
