64ed80b1fdb657c2cd4e8b257ae0eeda282f007b
NathanJFeldman <nathanfeldman.init@gmail.com>
2024-10-30 13:24:10 +0100

    new

--- tcp/server.py
+++ tcp/server.py
@@ -4,29 +4,23 @@ HOST = "127.0.0.1"
 PORT = 12345
 
 clients : list = []
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.close()
+    def hantera_client(client):
+        while True:
+            try:
+                message = client.recv(1024)
+                for client in clients:
+                    client.send(message)
 
-def send_messages(message):
-    for client in clients:
-        client.send(message)
-
-def hantera_client(client):
-    while True:
-        try:
-            message = client.recv(1024)
-            send_messages(message)
-        except:
-            clients.remove(client)
-
-def chat_room():
-    s.bind((HOST, PORT))
-    s.listen(2)
-    client, adress = s.accept()
-    while True:
-        print(f"Connected with: {adress}")
-        thread = threading.Thread(target=hantera_client, args=(client,))
-        thread.start()
-
-if __name__ == "__main__":
-    chat_room() 
+            except:
+                clients.remove(client)
+    def chat_room():
+        s.bind((HOST, PORT))
+        s.listen(2)
+        client, adress = s.accept()
+        while True:
+            print(f"Connected with: {adress}")
+            thread = threading.Thread(target=hantera_client, args=(client,))
+            thread.start()
+    chat_room()
