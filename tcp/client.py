e44d479eea490972ddd3c1f65aa8e4f5d4f95934
NathanJFeldman <nathanfeldman.init@gmail.com>
2024-10-30 10:10:19 +0100

    new

--- tcp/client.py
+++ tcp/client.py
@@ -3,7 +3,15 @@ import socket, threading
 HOST = "127.0.0.1"
 PORT = 12345
 
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((HOST, PORT))
+    while True:
+        message = input("Enter message -> ")
+        server.sendall(message.encode("utf-8"))
+        data = server.recv(1024)
+        response = data.decode("utf-8")
+        print(f"Server response: {response}")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
-    
+if __name__ == "__main__":
+    main()
--- tcp/server.py
+++ tcp/server.py
@@ -4,8 +4,29 @@ HOST = "127.0.0.1"
 PORT = 12345
 
 chatters : list = []
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
-    sock.listen(10)
-    
+def client_handle():
+    while True:
+        data = server.recvfrom(1024)
+        if not data:
+            break
+        message = data.decode("utf-8")
+        response = "<{addr}>" + message
+        server.sendall(response.encode("utf-8"))
+
+def chat_room():
+
+    server.bind((HOST, PORT))
+    server.listen(5)
+    print(f"Connected to: {HOST}:{PORT}")
+
+    while True:
+        data, addr = server.accept()
+        print(f"Client connected from {addr}")
+
+        
+
+if __name__ == "__main__":
+    chat_room()
+
