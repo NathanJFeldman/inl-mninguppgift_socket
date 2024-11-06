c1fdac0aab2842c2ebeaf5fbdf8f574ca7619010
NathanJFeldman <nathanfeldman.init@gmail.com>
2024-11-06 18:23:03 +0100

    Update server.py
    udp

--- tcp/server.py
+++ tcp/server.py
@@ -1,28 +1,43 @@
-import socket, threading
+import socket, threading, queue
 
 HOST = "127.0.0.1"
 PORT = 12345
 
-clients : list = []
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # Enable broadcasting mode
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    def hantera_client(client):
-        while True:
-            try:
-                message = client.recv(1024)
-                for client in clients:
-                    client.send(message)
+messages = queue.Queue
 
-            except:
-                clients.remove(client)
-    def chat_room():
-        s.bind((HOST, PORT))
-        s.listen(2)
-        client, adress = s.accept()
-        while True:
-            print(f"Connected with: {adress}")
-            thread = threading.Thread(target=hantera_client, args=(client,))
-            thread.start()
-    chat_room()
+clients = []
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((HOST, PORT))
+
+def recieve():
+    while True:
+        try:
+            message, addr = server.recvfrom(1024)
+            messages.put((message, addr))
+        except:
+            pass
+
+def broadcast():
+    while True:
+        while not messsage.empty():
+            message, addr = messages.get()
+            print(message.decode("utf-8"))
+            if addr not in clients:
+                clients.append(addr)
+            for client in clients:
+                try:
+                    if message.decode("utf-8").startswith("SIGNUP_TAG:"):
+                        name = message.decode()[message.decode().indexs(":")+1:]
+                        server.sendto(f"{name} har gått in!".encode(), client)
+                    else:
+                        server.sendto(message, client)
+                except:
+                    client.remove(client)
+
+t1 = threading.Thread(traget=receive)
+t2 = threading.Thread(target=broadcast)
+
+t1.start()
+t2.start()
